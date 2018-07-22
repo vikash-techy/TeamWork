8b9502cd9120056907eb10dae585a093a8028a80
Surya <Surya@Infected>
2018-07-22 12:08:55 +0530

    student architect service pilot

---
+++ stundent_architect_service/src/main/java/com/teamwork/stundent_architect_service/StundentArchitectServiceApplication.java
@@ -0,0 +1,12 @@
+package com.teamwork.stundent_architect_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StundentArchitectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StundentArchitectServiceApplication.class, args);
+	}
+}
---
+++ stundent_architect_service/src/test/java/com/teamwork/stundent_architect_service/StundentArchitectServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.teamwork.stundent_architect_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StundentArchitectServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
