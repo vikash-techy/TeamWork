7375738f397cd5a39183e553014bc19c8e0741f8
ankitkumar.jaishwal <ankitkumar.jaishwal@servicenow.com>
2018-08-19 14:27:54 +0530

    Added Subject and SVG Class Structure and formatted CSS file.

---
+++ StudentArchitect/src/app/subject.ts
@@ -0,0 +1,8 @@
+import { SVG } from './svg';
+export class Subject {
+    subjectId: number;
+    name: string;
+    videosCount: number;
+    goalsCount: number;
+    svgImage: SVG;
+  }
---
+++ StudentArchitect/src/app/svg.ts
@@ -0,0 +1,4 @@
+export class SVG {
+    path: string;
+    viewBoxCoOrdinates: string;
+}
